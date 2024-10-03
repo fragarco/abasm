4d193d64afcc3c9f7ad49f035af7c72d2d270eec
Javier Garcia <fragarco@gmail.com>
2024-10-03 19:49:29 +0200

    version 1.0

--- src/abasm.py
+++ src/abasm.py
@@ -20,7 +20,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 __author__='Javier "Dwayne Hicks" Garcia'
-__version__='1.0rc'
+__version__='1.0'
 
 import sys, os
 import re
