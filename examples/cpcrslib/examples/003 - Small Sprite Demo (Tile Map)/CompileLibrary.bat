e2d9977cbe616a191667cc98f029f19c18f1614f
Javier Garcia <fragarco@gmail.com>
2025-12-17 20:54:31 +0100

    remove extra files

--- examples/cpcrslib/examples/003 - Small Sprite Demo (Tile Map)/CompileLibrary.bat
+++
@@ -1,4 +0,0 @@
-copy ".\SDCC_config\*.*" "..\..\src\tilemap\"
-cd "..\..\src\"
-call make.bat
-cd "..\examples\003 - Small Sprite Demo (Tile Map)\"
--- examples/cpcrslib/examples/011 - Small Sprite Demo - SCROLL/CompileLibrary.bat
+++
@@ -1,4 +0,0 @@
-copy ".\SDCC_config\*.*" "..\..\src\tilemap\"
-cd "..\..\src\"
-call make.bat
-cd "..\examples\011 - Small Sprite Demo (Tile Map) - SCROLL\"
