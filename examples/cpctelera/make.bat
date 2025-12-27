f95f503f72252883ed70fe256db2d8308704c64a
fragarco <fragarco@gmail.com>
2025-12-27 23:43:22 +0100

    fix cpctelera examples for Windows

--- examples/cpctelera/make.bat
+++ examples/cpctelera/make.bat
@@ -10,8 +10,6 @@ REM * USAGE: make [clear]
 
 set ASM=python3 ../../src/abasm.py %*
 set DSK=python3 ../../src/dsk.py
-
-set LOADADDR=0x2000
 set TARGET=cpctelera
 
 IF "%1"=="clear" (
@@ -21,8 +19,11 @@ IF "%1"=="clear" (
     del *.dsk
 ) ELSE (
     call %DSK% %TARGET%.dsk --new
-    for %%x in (e01hello, e02box, e03struc, e04hflip, e05flipm, e06card) do (
-        call %ASM% --start %LOADADDR% %%x.asm && call %DSK% %TARGET%.dsk --put-bin %%x.bin --load-addr=%LOADADDR% --start-addr=%LOADADDR%
+    for %%x in (e01hello, e02box, e03struc, e04hflip) do (
+        call %ASM% --start 0x4000 %%x.asm && call %DSK% %TARGET%.dsk --put-bin %%x.bin --load-addr=0x4000 --start-addr=0x4000
+    )
+    for %%x in (e05flipm, e06card) do (
+        call %ASM% --start 0x2000 %%x.asm && call %DSK% %TARGET%.dsk --put-bin %%x.bin --load-addr=0x2000 --start-addr=0x2000
     )
 )
 
